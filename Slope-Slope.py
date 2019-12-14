cff7b0f940bb482e13802a4049b29e9e631190ae
AdinAck <adinackerman@gmail.com>
2019-12-13 22:25:46 -0800

    ...

--- Slope-Slope.py
+++ Slope-Slope.py
@@ -1,3 +1,9 @@
+# Problem 1: Slope-slope
+# Adin Ackerman solution
+# This script will return the slope of a line reflected across another line
+#   with only the slopes of the first 2 lines specified
+
+# With fractions
 from fractions import Fraction; a = [0,0]; b = [0,0]
 a[0] = input("Slope of line of reflection: "); a[1] = input("Slope of line\
  being reflected: ")
@@ -10,5 +16,6 @@ for j in range(2):
 print(Fraction(((2*b[0])/((1/b[0])+b[0])-(b[1]/((1/b[0])+b[1])))/\
 (2/((1/b[0])+b[0])-(1/((1/b[0])+b[1])))).limit_denominator())
 
+# Without fractions
 m = tuple(input("m1: ")); m2 = float(input("m2: "))
 print(((2*m1)/((1/m1)+m1)-(m2/((1/m1)+m2)))/(2/((1/m1)+m1)-(1/((1/m1)+m2))))
