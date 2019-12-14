5429ea8d7434f1779868029c4bad49baf14fe2fb
AdinAck <adinackerman@gmail.com>
2019-12-13 18:07:57 -0800

    ...

--- Slope-Slope.py
+++ Slope-Slope.py
@@ -1,9 +1,14 @@
 from fractions import Fraction; a = [0,0]; b = [0,0]
-a[0] = input("Slope of line of reflection: "); a[1] = input("Slope of line being reflected: ")
+a[0] = input("Slope of line of reflection: "); a[1] = input("Slope of line\
+ being reflected: ")
 for j in range(2):
     if "/" in a[j]:
         for i in range(len(a[j])):
             if "/" in a[j][i]: x = a[j][:i]; y = a[j][i+1:]
         b[j] = float(x)/float(y)
     else: b[j] = float(a[j])
-print("m3: "+str(Fraction(((2*b[0])/((1/b[0])+b[0])-(b[1]/((1/b[0])+b[1])))/(2/((1/b[0])+b[0])-(1/((1/b[0])+b[1])))).limit_denominator()))
+print(Fraction(((2*b[0])/((1/b[0])+b[0])-(b[1]/((1/b[0])+b[1])))/\
+(2/((1/b[0])+b[0])-(1/((1/b[0])+b[1])))).limit_denominator())
+
+m = tuple(input("m1, m2: "))
+print(((2*m[0])/((1/m[0])+m[0])-(m[1]/((1/m[0])+m[1])))/(2/((1/m[0])+m[0])-(1/((1/m[0])+m[1]))))
