48e8f0c719818c28f4c863cd300b15c4df6972a2
AdinAck <adinackerman@gmail.com>
2020-04-24 12:35:25 -0700

    ...

--- Google Meet Bot/Tesseract.py
+++ Google Meet Bot/Tesseract.py
@@ -3,9 +3,9 @@ import pytesseract
 import cv2
 from PIL import ImageGrab
 
-def imToString():
+def imToString(x1,y1,x2,y2):
     pytesseract.pytesseract.tesseract_cmd ='Tesseract/tesseract.exe'
-    cap = ImageGrab.grab(bbox =(396, 240, 396+417, 240+42))
+    cap = ImageGrab.grab(bbox =(x1,y1,x2,y2))
     tesstr = pytesseract.image_to_string(
             cv2.cvtColor(np.array(cap), cv2.COLOR_BGR2GRAY),
             lang ='eng')
--- Google Meet Bot/main.py
+++ Google Meet Bot/main.py
@@ -1,7 +1,7 @@
 import cleverbot as c
 import Tesseract as t
 c.open()
-string = t.imToString()
+string = t.imToString(396, 240, 396+417, 240+42)
 print("Sending",string)
 print("Received",c.chat(string))
 c.close()
