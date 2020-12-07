3e869fe29e1767d874a5a3145094f2ceab5e1ebd
AdinAck <adinackerman@gmail.com>
2020-12-06 21:22:04 -0800

    ...

--- chemistry_equation_balancer.py
+++ chemistry_equation_balancer.py
@@ -1,5 +1,9 @@
+# Chemistry equation balancer
+# By Adin Ackerman
+# https://github.com/AdinAck
+
 import numpy as np
-from sympy import *
+from sympy import Matrix, fraction
 
 def printMsg(msg):
     print("\n/////////////////////////\n")
@@ -16,7 +20,7 @@ for i in n:
     char = eq[i]
     if char not in exclude:
         if char == char.upper():
-            if eq[i-1] not in ["(", " "]:
+            if eq[i-1] not in "( ":
                 eq = eq[:i] + " " + eq[i:]
                 n += [len(eq)-1]
 
